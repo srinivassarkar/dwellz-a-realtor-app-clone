b243159b1055ab02fd25fbbc5be9ed36237a2426
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-09-10 22:08:23 +0530

    created header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useLocation, useNavigate } from "react-router";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  //console.log(location.pathname)
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
   return (
-    <div>
-        <img src="" alt="" />
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="../assets/og-logo.png"
+            alt="logo"
+            className="h-9 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              } `}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign In
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
