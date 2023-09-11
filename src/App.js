8486e9e618141f34c198d34f7992f66ecf43cbcf
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-09-11 05:43:26 +0530

    created sign in, sign up and forgot password pages and oauth component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <>
       <Router>
       <Header/>
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
