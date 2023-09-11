80cccb6fd388fef0dde83f6ae1adfff67c1a1445
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-09-12 00:51:13 +0530

    create sign in functionality with firebase

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import OAuth from "../Components/OAuth";
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
 
 export default function SignIn() {
+  const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -17,6 +22,25 @@ export default function SignIn() {
       [e.target.id]: e.target.value,
     }));
   }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try{
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth,email,password)
+      if (userCredential.user){
+        navigate("/")
+      }
+
+    }
+    catch (error){
+      toast.error("Bad user credentials")
+
+    }
+
+  }
+
+
 
   return (
     <section>
@@ -32,7 +56,7 @@ export default function SignIn() {
         </div>
 
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
