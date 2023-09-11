4e325c114ddc83cf4dbe6864c586098593cb2e55
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-09-12 00:14:43 +0530

    complete the OAuth functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
-                pathMathRoute("/") && "text-black border-b-red-500"
+                pathMathRoute("/") && "text-black border-b-red-600"
               }`}
               onClick={() => navigate("/")}
             >
@@ -33,7 +33,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
-                pathMathRoute("/offers") && "text-black border-b-red-500"
+                pathMathRoute("/offers") && "text-black border-b-red-600"
               } `}
               onClick={() => navigate("/offers")}
             >
@@ -41,7 +41,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400  border-b-[3px] border-b-transparent ${
-                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+                pathMathRoute("/sign-in") && "text-black border-b-red-600"
               }`}
               onClick={() => navigate("/sign-in")}
             >
--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { db } from "./../firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
+  async function onGoogleClick() {
+    if (isSigningIn) {
+      // Prevent multiple sign-in attempts.
+      return;
+    }
+
+    setIsSigningIn(true);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      const docRef = doc(db, "users", user.uid);
+
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    } finally {
+      setIsSigningIn(false);
+    }
+  }
+
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2"/>
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      disabled={isSigningIn}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
   );
