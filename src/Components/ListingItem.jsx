6bcb7704beb6528a64b460c5cea6ee943b914541
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-10-02 03:51:04 +0530

    Complete the offers page

--- src/Components/ListingItem.jsx
+++ src/Components/ListingItem.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 import { FaTrash } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+
+
+
 export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
